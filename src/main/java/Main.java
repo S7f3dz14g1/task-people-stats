1dac9c2efebddcc4f6975491e70403ca5b786732
Krystian Szwedo <krystainszwedo@wp.pl>
2020-10-19 16:28:42 +0200

    added method count and

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,12 @@
+import java.nio.file.Paths;
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
+        System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
     }
 }
 
-class PeopleStats {
 
-}
 
-class Person {
-    private final String firstName;
-    private final String lastName;
-
-    public Person(String firstName,String lastName) {
-        this.firstName = firstName;
-        this.lastName=lastName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-}
